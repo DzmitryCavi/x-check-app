378734f9ae3290f6cd5da27205436e56d1fe4dcf
hardz <hardzeichyksiarhei@gmail.com>
2020-09-02 21:50:58 +0300

    refactor: use class component in Login.jsx

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,78 +1,87 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import axios from 'axios'
 
-const Login = (props) => {
-  const [data, setData] = useState({ errorMessage: '', isLoading: false })
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
 
-  const { isLoggedIn, clientId, clientSecret, redirectURI, proxyURL, dispatch } = props
+    this.state = {
+      isLoading: false,
+      errorMessage: '',
+    }
+  }
 
-  useEffect(() => {
-    const url = window.location.href
-    const hasCode = url.includes('?code=')
+  async componentDidMount() {
+    const { clientId, clientSecret, redirectURI, proxyURL, dispatch } = this.props
+
+    const uri = window.location.href
+    const hasCode = uri.includes('?code=')
 
     if (hasCode) {
-      const newUrl = url.split('?code=')
-      window.history.pushState({}, null, newUrl[0])
-      setData({ ...data, isLoading: true })
+      const [url, code] = uri.split('?code=')
+      window.history.pushState({}, null, url)
+      this.setState((prev) => ({ ...prev, isLoading: true }))
 
       const requestData = {
         clientId,
         redirectURI,
         clientSecret,
-        code: newUrl[1],
+        code,
       }
 
-      fetch(proxyURL, {
-        method: 'POST',
-        body: JSON.stringify(requestData),
-      })
-        .then((response) => response.json())
-        // eslint-disable-next-line no-shadow
-        .then((data) => {
-          dispatch({
-            type: 'LOGIN',
-            payload: { user: data, isLoggedIn: true },
-          })
+      try {
+        const { data } = await axios.post(proxyURL, requestData)
+        dispatch({
+          type: 'LOGIN',
+          payload: { user: data, isLoggedIn: true },
         })
-        .catch(() => {
-          setData({
-            isLoading: false,
-            errorMessage: 'Sorry! Login failed',
-          })
+      } catch (error) {
+        this.setState({
+          isLoading: false,
+          errorMessage: 'Sorry! Login failed',
         })
+      }
     }
-  }, [clientId, clientSecret, data, dispatch, proxyURL, redirectURI])
-
-  if (isLoggedIn) {
-    return <Redirect to="/" />
   }
 
-  return (
-    <section className="container">
-      <div>
-        <h1>Login</h1>
-        <span>Super amazing app</span>
-        <span>{data.errorMessage}</span>
-        <div className="login-container">
-          {data.isLoading ? (
-            <div>Loading...</div>
-          ) : (
-            <a
-              className="login-link"
-              href={`https://github.com/login/oauth/authorize?scope=user&client_id=${clientId}&redirect_uri=${redirectURI}`}
-              onClick={() => {
-                setData({ ...data, errorMessage: '' })
-              }}
-            >
-              <span>Login with GitHub</span>
-            </a>
-          )}
+  handleLogin = () => {
+    this.setState((prev) => ({ ...prev, errorMessage: '' }))
+  }
+
+  render() {
+    const { isLoggedIn, clientId, redirectURI } = this.props
+    const { isLoading, errorMessage } = this.state
+
+    if (isLoggedIn) {
+      return <Redirect to="/" />
+    }
+
+    return (
+      <section className="container">
+        <div>
+          <h1>Login</h1>
+          <span>Super amazing app</span>
+          <span>{errorMessage}</span>
+          <div className="login-container">
+            {isLoading ? (
+              <div>Loading...</div>
+            ) : (
+              <a
+                className="login-link"
+                href={`https://github.com/login/oauth/authorize?scope=user&client_id=${clientId}&redirect_uri=${redirectURI}`}
+                onClick={this.handleLogin}
+              >
+                <span>Login with GitHub</span>
+              </a>
+            )}
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
 
 Login.propTypes = {
