ab401d1b54f7a7123feecfe8ba86157889c5f00a
hardz <hardzeichyksiarhei@gmail.com>
2020-09-03 12:26:04 +0300

    fix: component -> containers

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import LoginForm from '../component/LoginForm/LoginForm'
+import LoginForm from '../containers/LoginForm/LoginForm'
 
 const Login = () => {
   return (
