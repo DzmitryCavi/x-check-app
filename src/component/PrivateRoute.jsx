9e3c273ca2035d552c7bb4b4f89b488b5195d46e
hardz <hardzeichyksiarhei@gmail.com>
2020-09-05 02:25:39 +0300

    feat: add not access notification

--- src/component/PrivateRoute.jsx
+++ src/component/PrivateRoute.jsx
@@ -1,20 +1,31 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { notification } from 'antd'
 
 const PrivateRoute = ({ component: Component, isLoggedIn, user, allowedRoles, ...rest }) => {
   const isAuth = isLoggedIn && user
   const isAccess = isAuth && (allowedRoles.includes(user.role) || !allowedRoles.length)
 
   const ResultComponent = (props) => {
+    useEffect(() => {
+      if (!isAccess) {
+        notification.error({
+          className: 'app-notification app-notification--error',
+          message: 'Error',
+          description: 'What are you doing... You do not have access to this page!',
+        })
+      }
+    }, [])
+
     if (!isAuth) return <Redirect to="/login" />
-    if (!isAccess) return <Redirect to="/" />
+    if (!isAccess) return <Redirect to={{ pathname: '/', state: { isNotAccess: true } }} />
     return <Component {...props} />
   }
 
-  return <Route {...rest} render={ResultComponent} />
+  return <Route {...rest} component={ResultComponent} />
 }
 
 PrivateRoute.defaultProps = {
