e81a1bd94ea616d7a18b7610aa7e63d8f77020c4
hardz <hardzeichyksiarhei@gmail.com>
2020-09-04 16:21:35 +0300

    feat: redesign DefaultLayout

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,54 +1,104 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-import { Layout, Menu } from 'antd'
-import { UserOutlined, LaptopOutlined, LogoutOutlined } from '@ant-design/icons'
+import { Link, Redirect } from 'react-router-dom'
+import { Layout, Menu, Breadcrumb, Button, Avatar, Dropdown } from 'antd'
+import { UserOutlined, LaptopOutlined, LogoutOutlined, ProfileOutlined } from '@ant-design/icons'
 import { connect } from 'react-redux'
 import { logout } from '../store/actions'
 
-const { Header, Content, Sider } = Layout
+const { Header, Content, Sider, Footer } = Layout
 
-const DefaultLayout = ({ children, dispatch }) => {
+const DefaultLayout = ({ isLoggedIn, user, children, dispatch }) => {
+  const exit = () => {
+    dispatch(logout())
+  }
+
+  const ProfileMenu = (
+    <Menu>
+      <Menu.Item key="1" icon={<ProfileOutlined />}>
+        Profile
+      </Menu.Item>
+      <Menu.Item key="2" icon={<LogoutOutlined />} onClick={exit}>
+        Logout
+      </Menu.Item>
+    </Menu>
+  )
+
+  if (!isLoggedIn) {
+    return <Redirect to="/login" />
+  }
+
   return (
     <Layout className="default-layout">
-      <Header className="default-layout__header">
-        <h1 className="default-layout__logo">X Check App</h1>
-      </Header>
-      <Layout>
-        <Sider width={320} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            style={{ height: '100%', borderRight: 0 }}
-          >
-            <Menu.Item icon={<UserOutlined />} key="1">
-              <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item icon={<LaptopOutlined />} key="2">
-              <Link to="/private">Private</Link>
-            </Menu.Item>
-            <Menu.Item
-              icon={<LogoutOutlined />}
-              key="3"
-              onClick={() => {
-                dispatch(logout())
-              }}
+      <Header className="app-header">
+        <div className="app-header__logo">X Check App</div>
+        <div className="app-header__controls">
+          <Dropdown overlay={ProfileMenu}>
+            <Button
+              type="dashed"
+              size="large"
+              icon={
+                <Avatar
+                  style={{
+                    width: 24,
+                    height: 24,
+                    lineHeight: '24px',
+                    fontSize: 18,
+                    marginTop: -4,
+                    marginRight: 10,
+                  }}
+                  size="small"
+                  src={user.avatar_url}
+                />
+              }
             >
-              Logout
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
+              My Profile
+            </Button>
+          </Dropdown>
+        </div>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb className="default-layout__breadcrumbs">
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout style={{ padding: '24px 0', backgroundColor: '#fff' }}>
+          <Sider width={320}>
+            <Menu mode="inline" defaultSelectedKeys={['1']} style={{ height: '100%' }}>
+              <Menu.Item icon={<UserOutlined />} key="1">
+                <Link to="/">Home</Link>
+              </Menu.Item>
+              <Menu.Item icon={<LaptopOutlined />} key="2">
+                <Link to="/private">Private</Link>
+              </Menu.Item>
+              <Menu.Item icon={<LogoutOutlined />} key="3" onClick={exit}>
+                Logout
+              </Menu.Item>
+            </Menu>
+          </Sider>
           <Content className="default-layout__content">{children}</Content>
         </Layout>
-      </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
     </Layout>
   )
 }
 
+DefaultLayout.defaultProps = {
+  user: null,
+}
+
 DefaultLayout.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  user: PropTypes.instanceOf(Object),
   children: PropTypes.instanceOf(Object).isRequired,
   dispatch: PropTypes.func.isRequired,
 }
 
-export default connect()(DefaultLayout)
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.auth.isLoggedIn,
+    user: state.auth.user,
+  }
+}
+
+export default connect(mapStateToProps, null)(DefaultLayout)
