1de16c33a266ae5579f7b6eadfb2856b47d07751
hardz <hardzeichyksiarhei@gmail.com>
2020-09-06 20:17:41 +0300

    feat: add tasks.service.js

---
+++ src/services/tasks.service.js
@@ -0,0 +1,19 @@
+// import axios from '../plugins/axios'
+
+const getAll = async () => {}
+
+// const getById = async (id) => {}
+
+// const create = async (task) => {}
+
+// const edit = async (task) => {}
+
+// const destroyById = async (id) => {}
+
+export default {
+  getAll,
+  //   getById,
+  //   create,
+  //   edit,
+  //   destroyById,
+}
