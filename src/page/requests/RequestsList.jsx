cc80eb34cc1a343c50567df46f965fb97372d0c6
lugovczov <vova.lugovczov@mail.ru>
2020-09-13 19:36:02 +0300

    feat: add review request table

--- src/page/requests/RequestsList.jsx
+++ src/page/requests/RequestsList.jsx
@@ -1,47 +1,86 @@
-import React from 'react'
-import { List, Avatar } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { Table, Tag } from 'antd'
 import { EditOutlined } from '@ant-design/icons'
 
+import Axios from 'axios'
+import Column from 'antd/lib/table/Column'
 import ButtonLink from '../../component/ButtonLink'
 
-const data = [
-  {
-    title: 'Task 1',
-  },
-  {
-    title: 'Task 2',
-  },
-  {
-    title: 'Task 3',
-  },
-  {
-    title: 'Task 4',
-  },
-]
-
 const RequestsList = () => {
+  const [infoTask, setInfoTask] = useState()
+
+  useEffect(() => {
+    // Axios.get(`${REACT_APP_API_URL}/reviewRequest`)
+    Axios.get('http://localhost:50005/reviewRequest')
+      .then((resp) => {
+        const { data } = resp
+        setInfoTask(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  useEffect(() => {
+    setInfoTask(infoTask)
+  }, [infoTask])
+
   return (
-    <List
-      itemLayout="horizontal"
-      dataSource={data}
-      renderItem={(item) => (
-        <List.Item
-          actions={[
-            <ButtonLink type="primary" icon={<EditOutlined />} linkTo={`/request/${item.title}`}>
-              Review
-            </ButtonLink>,
-          ]}
-        >
-          <List.Item.Meta
-            avatar={
-              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            }
-            title={<a href="https://ant.design">{item.title}</a>}
-            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-          />
-        </List.Item>
-      )}
-    />
+    <Table dataSource={infoTask} pagination={{ pageSize: 10 }} rowKey="author">
+      <Column
+        title="Name"
+        dataIndex="author"
+        key="author"
+        sorter={(a, b) => a.author.localeCompare(b.author)}
+      />
+      <Column
+        title="Task"
+        dataIndex="task"
+        key="task"
+        sorter={(a, b) => a.task.localeCompare(b.task)}
+      />
+      <Column
+        title="Status"
+        dataIndex="state"
+        key="state"
+        sorter={(a, b) => a.state.localeCompare(b.state)}
+        render={(state) => {
+          let color
+          if (state === 'PUBLISHED') {
+            color = 'green'
+          } else if (state === 'COMPLETED') {
+            color = 'blue'
+          } else if (state === 'DRAFT') {
+            color = 'red'
+          } else {
+            color = 'blue'
+          }
+
+          return (
+            <>
+              <Tag color={color} key={state}>
+                {state}
+              </Tag>
+            </>
+          )
+        }}
+      />
+
+      <Column
+        title="Action"
+        key="action"
+        render={() => (
+          <ButtonLink
+            type="primary"
+            icon={<EditOutlined />}
+            linkTo="/review"
+            key={`${'action'}-btn`}
+          >
+            Review
+          </ButtonLink>
+        )}
+      />
+    </Table>
   )
 }
 
