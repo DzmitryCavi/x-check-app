d337de802bb6dd3e8b4fd6f9be843063f0d96303
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-09-13 19:24:56 +0300

    feat: add filters to tasks table

--- src/page/author/tasks/TasksList.jsx
+++ src/page/author/tasks/TasksList.jsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { formatRoute } from 'react-router-named-routes'
-import { Table, Space, Button, Tag, notification } from 'antd'
-import { DeleteOutlined, EditOutlined, PlusOutlined, EyeOutlined } from '@ant-design/icons'
+import { Form, Radio, Input, Row, Col, Table, Space, Button, Tag, notification } from 'antd'
+import {
+  DeleteOutlined,
+  DownOutlined,
+  EditOutlined,
+  PlusOutlined,
+  UpOutlined,
+} from '@ant-design/icons'
 import ButtonLink from '../../../component/ButtonLink'
 import { authorRoutes } from '../../../router/routes'
 
@@ -13,11 +19,22 @@ import tasksService from '../../../services/tasks.service'
 const { Column } = Table
 
 const TasksList = ({ user }) => {
+  const initTasks = useRef([])
+
   const [tasks, setTasks] = useState([])
   const [loading, setLoading] = useState(true)
 
+  const [filtersForm] = Form.useForm()
+  const [expandFilter, setExpandFilter] = useState(false)
+
+  const filters = {
+    title: (title, value) => title.toLowerCase().indexOf(value.toLowerCase()) !== -1,
+    state: (state, value) => state === value,
+  }
+
   useEffect(() => {
     tasksService.getAllByAuthorId(user.id).then((data) => {
+      initTasks.current = data
       setTasks(data)
       setLoading(false)
     })
@@ -34,16 +51,90 @@ const TasksList = ({ user }) => {
     })
   }
 
+  const onFilter = (filterData) => {
+    setTasks(
+      initTasks.current.filter((row) =>
+        Object.keys(filterData).every((key) =>
+          typeof filters[key] !== 'function' ? true : filters[key](row[key], filterData[key]),
+        ),
+      ),
+    )
+  }
+
+  const onClearFilter = () => {
+    setTasks(initTasks.current)
+    filtersForm.resetFields()
+  }
+
   return (
     <div className="tasks-list-page">
       <h1 className="page-title">Tasks</h1>
+      <div className="tasks-filters">
+        <h2 className="tasks-filters__title">Filters:</h2>
+        <Form
+          form={filtersForm}
+          layout="vertical"
+          onFinish={onFilter}
+          initialValues={{
+            title: '',
+            state: '',
+          }}
+        >
+          <Row gutter={30}>
+            {expandFilter ? (
+              <>
+                <Col span={6}>
+                  <Form.Item name="title" label="Title">
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col span={6}>
+                  <Form.Item label="State" name="state">
+                    <Radio.Group>
+                      <Radio.Button value="DRAFT">DRAFT</Radio.Button>
+                      <Radio.Button value="PUBLISHED">PUBLISHED</Radio.Button>
+                      <Radio.Button value="ARCHIVED">ARCHIVED</Radio.Button>
+                    </Radio.Group>
+                  </Form.Item>
+                </Col>
+              </>
+            ) : null}
+            <Col span={24}>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" disabled={!expandFilter}>
+                  Filter
+                </Button>
+                <Button style={{ margin: '0 8px' }} onClick={onClearFilter}>
+                  Clear
+                </Button>
+                <Button
+                  style={{ fontSize: 12 }}
+                  type="link"
+                  onClick={() => {
+                    setExpandFilter(!expandFilter)
+                  }}
+                >
+                  {!expandFilter ? <UpOutlined /> : <DownOutlined />} Collapse
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </div>
       <div className="d-flex justify-content-end align-items-center mb-2">
         <ButtonLink type="primary" icon={<PlusOutlined />} linkTo={authorRoutes.tasks.create}>
           Create
         </ButtonLink>
       </div>
       <Table dataSource={tasks} rowKey="id" loading={loading}>
-        <Column width={60} title="#" dataIndex="id" key="id" sorter={(a, b) => a.id - b.id} />
+        <Column
+          width={60}
+          title="#"
+          dataIndex="id"
+          key="id"
+          sorter={(a, b) => a.id - b.id}
+          defaultSortOrder="descend"
+        />
         <Column
           title="Title"
           key="title"
@@ -79,12 +170,12 @@ const TasksList = ({ user }) => {
                 Category
               </ButtonLink>
 
-              <ButtonLink
+              {/* <ButtonLink
                 icon={<EyeOutlined />}
                 linkTo={formatRoute(authorRoutes.tasks.view, { taskId: row.id })}
               >
                 View
-              </ButtonLink>
+              </ButtonLink> */}
 
               <ButtonLink
                 icon={<EditOutlined />}
