de1da284a3112ce640c977914e057112a6dc14a6
DzmitryCavi <dzmitry.malakhouski@tut.by>
2020-09-13 21:12:16 +0300

    fix:  ReviewFormItem component errors

--- src/component/forms/ReviewFormItem.jsx
+++ src/component/forms/ReviewFormItem.jsx
@@ -1,47 +1,38 @@
 import React, { useState } from 'react'
-import { Form, Select, Button, InputNumber } from 'antd'
+import { InputNumber, Input, Slider } from 'antd'
 import PropTypes from 'prop-types'
 
-const { Option } = Select
-
-const Score = ({ value = {}, onChange }) => {
+const ReviewFormItem = ({ value, onChange }) => {
   const [number, setNumber] = useState(0)
-  const [currency, setCurrency] = useState('rmb')
-
+  const [discription, setDiscription] = useState('')
   const triggerChange = (changedValue) => {
     if (onChange) {
       onChange({
         number,
-        currency,
+        discription,
         ...value,
         ...changedValue,
       })
     }
   }
 
-  const onNumberChange = (e) => {
-    const newNumber = parseInt(e.target.value || 0, 10)
-
-    if (Number.isNaN(number)) {
+  const onNumberChange = (score) => {
+    const newNumber = score
+    if (Number.isNaN(score)) {
       return
     }
 
-    if (!('number' in value)) {
-      setNumber(newNumber)
-    }
+    setNumber(newNumber)
 
     triggerChange({
       number: newNumber,
     })
   }
 
-  const onCurrencyChange = (newCurrency) => {
-    if (!('currency' in value)) {
-      setCurrency(newCurrency)
-    }
-
+  const onDiscriptionChange = (e) => {
+    setDiscription(e.target.value)
     triggerChange({
-      currency: newCurrency,
+      discription: e.target.value,
     })
   }
 
@@ -54,53 +45,33 @@ const Score = ({ value = {}, onChange }) => {
           width: 100,
         }}
       />
-      <Select
-        value={value.currency || currency}
+      <Slider
+        min={0}
+        max={40}
+        marks={{ 0: 'min', 20: 'half', 40: 'max' }}
+        onChange={onNumberChange}
+        value={typeof value.number === 'number' ? value.number : 0}
+      />
+      <Input.TextArea
+        value={value.discription || discription}
         style={{
           width: 80,
           margin: '0 8px',
         }}
-        onChange={onCurrencyChange}
-      >
-        <Option value="rmb">RMB</Option>
-        <Option value="dollar">Dollar</Option>
-      </Select>
+        onChange={onDiscriptionChange}
+      />
     </span>
   )
 }
 
-const ReviewFormItem = () => {
-  const onFinish = (values) => {
-    console.log('Received values from form: ', values)
-  }
-
-  return (
-    <Form
-      name="customized_form_controls"
-      layout="inline"
-      onFinish={onFinish}
-      initialValues={{
-        price: {
-          number: 0,
-          currency: 'rmb',
-        },
-      }}
-    >
-      <Form.Item name="price" label="Price">
-        <Score />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  )
+ReviewFormItem.propTypes = {
+  value: PropTypes.instanceOf(Object),
+  onChange: PropTypes.instanceOf(Function),
 }
 
-Score.propTypes = {
-  value: PropTypes.instanceOf(Object).isRequired,
-  onChange: PropTypes.instanceOf(Function).isRequired,
+ReviewFormItem.defaultProps = {
+  value: {},
+  onChange: () => {},
 }
 
 export default ReviewFormItem
--- src/component/forms/SelfReview.jsx
+++ src/component/forms/SelfReview.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useRef, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Form, notification, Spin } from 'antd'
+import { Form, notification, Spin, Button } from 'antd'
+import ReviewFormItem from './ReviewFormItem'
 import categoriesService from '../../services/categories.service'
 
 const SelfReview = ({ task }) => {
@@ -10,8 +11,6 @@ const SelfReview = ({ task }) => {
   useEffect(() => {
     categoriesService.getAllByTaskId(task.id).then(setCategories)
   }, [task.id])
-
-  console.log(categories)
 
   const formRef = useRef(null)
 
@@ -23,18 +22,30 @@ const SelfReview = ({ task }) => {
     })
   }
 
+  const init = {
+    score:
+      categories && categories.items !== undefined
+        ? categories.items.reduce((ac, e) => {
+            const result = { ...ac }
+            result[e.id] = e
+            return result
+          }, {})
+        : {},
+    data: 232134234,
+  }
+
   return (
     <div className="task-create-page">
-      <Form ref={formRef} layout="vertical" onFinish={onFinish}>
+      <Form ref={formRef} layout="vertical" onFinish={onFinish} initialValues={init}>
         {categories ? (
           categories.map((category) => (
             <div key={category.id}>
               <div>{category.title}</div>
               {category.items.map((item) => (
                 <div key={item.id}>
-                  {`${item.description} (${item.minScore}-${item.maxScore})`}{' '}
-                  <Form.Item name={item.id} label="Slider">
-                    asdasd
+                  {`${item.description} (${item.minScore}-${item.maxScore})`}
+                  <Form.Item name={item.id} label={item.description}>
+                    <ReviewFormItem />
                   </Form.Item>
                 </div>
               ))}
@@ -43,6 +54,11 @@ const SelfReview = ({ task }) => {
         ) : (
           <Spin size="large" />
         )}
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
       </Form>
     </div>
   )
