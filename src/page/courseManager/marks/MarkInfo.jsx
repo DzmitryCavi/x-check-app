b0b824f2909c80e52a2172cf703ecd81bf37d3c7
Vladimir Kormakov <cenizo.kht@gmail.com>
2020-09-17 12:23:46 +0300

    refactor: course manager components with function

--- src/page/courseManager/marks/MarkInfo.jsx
+++ src/page/courseManager/marks/MarkInfo.jsx
@@ -1,64 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Button, Modal } from 'antd'
 import PropTypes from 'prop-types'
-import fakeData from './fakeData'
 
-export default class MarkInfo extends Component {
-  constructor() {
-    super()
-    this.state = {
-      visible: false,
-    }
+const MarkInfo = (props) => {
+  const [state, setState] = useState({
+    visible: false,
+  })
+
+  const showModal = () => {
+    setState({ ...state, visible: true })
   }
 
-  showModal = () => {
-    this.setState({
-      visible: true,
-    })
+  const handleCancel = () => {
+    setState({ ...state, visible: false })
   }
 
-  handleCancel = () => {
-    this.setState({
-      visible: false,
-    })
+  const { data } = props
+  const { task, maxScore, reviewer, info, student, score } = data
+  const { basicP1, extraP1, finesP1 } = info
+  const { visible } = state
+  const styleSheet = {
+    modalText: { fontWeight: '500', fontSize: '15px' },
   }
 
-  render() {
-    const { infoKey } = this.props
-    const [element] = fakeData.filter((el) => el.key === infoKey)
-    const { task, maxScore, reviewer, info, student, score } = element
-    const { basicP1, extraP1, finesP1 } = info
-    const { visible } = this.state
-    const styleSheet = {
-      modalText: { fontWeight: '500', fontSize: '15px' },
-    }
-
-    return (
-      <>
-        <Button type="primary" onClick={this.showModal}>
-          info
-        </Button>
-        <Modal
-          title={`Task: ${task}   |    Student: ${student}`}
-          visible={visible}
-          onCancel={this.handleCancel}
-          footer={[]}
-        >
-          <h3>reviewer: {reviewer}</h3>
-          <h4>score: {score}</h4>
-          <p style={styleSheet.modalText}>max_score: {maxScore}</p>
-          <p style={styleSheet.modalText}>basic scope: {basicP1.score}</p>
-          <p style={styleSheet.modalText}>extra scope: {extraP1.score}</p>
-          <p style={styleSheet.modalText}>comment: {basicP1.comment}</p>
-          <p style={styleSheet.modalText}>comment: {extraP1.comment}</p>
-          <p style={styleSheet.modalText}>comment: {finesP1.comment}</p>
-          <p style={styleSheet.modalText}>errors: {finesP1.score}</p>
-        </Modal>
-      </>
-    )
-  }
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        info
+      </Button>
+      <Modal
+        title={`Task: ${task}   |    Student: ${student}`}
+        visible={visible}
+        onCancel={handleCancel}
+        footer={[]}
+      >
+        <h3>reviewer: {reviewer}</h3>
+        <h4>score: {score}</h4>
+        <p style={styleSheet.modalText}>max_score: {maxScore}</p>
+        <p style={styleSheet.modalText}>basic scope: {basicP1.score}</p>
+        <p style={styleSheet.modalText}>extra scope: {extraP1.score}</p>
+        <p style={styleSheet.modalText}>comment: {basicP1.comment}</p>
+        <p style={styleSheet.modalText}>comment: {extraP1.comment}</p>
+        <p style={styleSheet.modalText}>comment: {finesP1.comment}</p>
+        <p style={styleSheet.modalText}>errors: {finesP1.score}</p>
+      </Modal>
+    </>
+  )
 }
 
 MarkInfo.propTypes = {
-  infoKey: PropTypes.number.isRequired,
+  data: PropTypes.instanceOf(Object).isRequired,
 }
+
+export default MarkInfo
--- src/page/courseManager/marks/MarksList.jsx
+++ src/page/courseManager/marks/MarksList.jsx
@@ -1,91 +1,84 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Table, Button, Space } from 'antd'
 import fakeData from './fakeData'
 import MarkInfo from './MarkInfo'
 
-class MarksList extends Component {
-  constructor() {
-    super()
-    this.state = {
-      filteredInfo: null,
-    }
+const MarksList = () => {
+  const [state, setState] = useState({
+    filteredInfo: null,
+  })
+
+  const handleChange = (pagination, filters) => {
+    setState({ ...state, filteredInfo: filters })
   }
 
-  handleChange = (filters) => {
-    this.setState({
-      filteredInfo: filters,
-    })
+  const clearFilters = () => {
+    setState({ ...state, filteredInfo: null })
   }
 
-  clearFilters = () => {
-    this.setState({ filteredInfo: null })
-  }
-
-  render() {
-    let { filteredInfo } = this.state
-    filteredInfo = filteredInfo || {}
-    const columns = [
-      {
-        title: 'Task',
-        dataIndex: 'task',
-        filters: [
-          { text: 'Songbird', value: 'Songbird' },
-          { text: 'X-check', value: 'X-check' },
-        ],
-        filteredValue: filteredInfo.task || null,
-        onFilter: (value, record) => record.task.includes(value),
+  let { filteredInfo } = state
+  filteredInfo = filteredInfo || {}
+  const columns = [
+    {
+      title: 'Task',
+      dataIndex: 'task',
+      filters: [
+        { text: 'Songbird', value: 'Songbird' },
+        { text: 'X-check', value: 'X-check' },
+      ],
+      filteredValue: filteredInfo.task || null,
+      onFilter: (value, record) => record.task.includes(value),
+    },
+    {
+      title: 'Student',
+      dataIndex: 'student',
+      filters: [
+        { text: 'multeng', value: 'multeng' },
+        { text: 'GurbanovAli', value: 'GurbanovAli' },
+      ],
+      filteredValue: filteredInfo.student || null,
+      onFilter: (value, record) => record.student.includes(value),
+    },
+    {
+      title: 'Reviewer',
+      dataIndex: 'reviewer',
+      filters: [
+        { text: 'ButterBrot777', value: 'ButterBrot777' },
+        { text: 'cardamo', value: 'cardamo' },
+      ],
+      filteredValue: filteredInfo.reviewer || null,
+      onFilter: (value, record) => record.reviewer.includes(value),
+    },
+    {
+      title: 'Score',
+      dataIndex: 'score',
+      sorter: {
+        compare: (a, b) => a.score - b.score,
+        multiple: 1,
       },
-      {
-        title: 'Student',
-        dataIndex: 'student',
-        filters: [
-          { text: 'multeng', value: 'multeng' },
-          { text: 'GurbanovAli', value: 'GurbanovAli' },
-        ],
-        filteredValue: filteredInfo.student || null,
-        onFilter: (value, record) => record.student.includes(value),
+    },
+    {
+      title: 'Max Score',
+      dataIndex: 'maxScore',
+      sorter: {
+        compare: (a, b) => a.maxScore - b.maxScore,
+        multiple: 1,
       },
-      {
-        title: 'Reviewer',
-        dataIndex: 'reviewer',
-        filters: [
-          { text: 'ButterBrot777', value: 'ButterBrot777' },
-          { text: 'cardamo', value: 'cardamo' },
-        ],
-        filteredValue: filteredInfo.reviewer || null,
-        onFilter: (value, record) => record.reviewer.includes(value),
-      },
-      {
-        title: 'Score',
-        dataIndex: 'score',
-        sorter: {
-          compare: (a, b) => a.score - b.score,
-          multiple: 1,
-        },
-      },
-      {
-        title: 'Max Score',
-        dataIndex: 'maxScore',
-        sorter: {
-          compare: (a, b) => a.maxScore - b.maxScore,
-          multiple: 1,
-        },
-      },
-      {
-        title: 'Action',
-        dataindex: 'action',
-        render: (row) => <MarkInfo infoKey={row.key} />,
-      },
-    ]
-    return (
-      <>
-        <Space style={{ marginBottom: 16 }}>
-          <Button onClick={this.clearFilters}>Clear filters</Button>
-        </Space>
-        <Table columns={columns} dataSource={fakeData} onChange={this.handleChange} />
-      </>
-    )
-  }
+    },
+    {
+      title: 'Action',
+      dataindex: 'action',
+      render: (row) => <MarkInfo data={row} />,
+    },
+  ]
+  return (
+    <>
+      <Space style={{ marginBottom: 16 }}>
+        <Button onClick={clearFilters}>Clear filters</Button>
+      </Space>
+      <Table columns={columns} dataSource={fakeData} onChange={handleChange} />
+    </>
+  )
 }
 
 export default MarksList
