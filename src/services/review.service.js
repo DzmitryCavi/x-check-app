2a2703108a489a433fcd41687c008be09031b391
DzmitryCavi <dzmitry.malakhouski@tut.by>
2020-09-26 23:46:14 +0300

    fix: course manager rmarks list

--- src/services/review.service.js
+++ src/services/review.service.js
@@ -8,6 +8,11 @@ const getAll = async () => {
   return status === 200 && tasks ? tasks : []
 }
 
+const getAllGraded = async () => {
+  const { data: tasks, status } = await axios.get(`${API_URL}/reviews?state=GRADED`)
+  return status === 200 && tasks ? tasks : []
+}
+
 const getById = async (id) => {
   const { data: task, status } = await axios.get(`${API_URL}/reviews/${id}`)
   return status === 200 && task ? task : null
@@ -43,6 +48,7 @@ const destroyById = async (id) => {
 }
 
 export default {
+  getAllGraded,
   getAll,
   create,
   edit,
