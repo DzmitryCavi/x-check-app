0ed0982259f4f10dbf6237c613d12712c9014938
DzmitryCavi <dzmitry.malakhouski@tut.by>
2020-10-02 22:51:38 +0300

    feat: add Feedback component for messaging

---
+++ src/component/Feedback.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Comment, Avatar, Form, Button, List, Input } from 'antd'
+import moment from 'moment'
+import { connect } from 'react-redux'
+
+const { TextArea } = Input
+
+const CommentList = ({ comments }) => (
+  <List
+    dataSource={comments}
+    header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
+    itemLayout="horizontal"
+    renderItem={(props) => <Comment {...props} />}
+  />
+)
+
+CommentList.propTypes = {
+  comments: PropTypes.instanceOf(Array).isRequired,
+}
+
+const Editor = ({ onChange, onSubmit, submitting, value }) => (
+  <>
+    <Form.Item>
+      <TextArea rows={4} onChange={onChange} value={value} />
+    </Form.Item>
+    <Form.Item>
+      <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+        Add Comment
+      </Button>
+    </Form.Item>
+  </>
+)
+
+Editor.propTypes = {
+  onChange: PropTypes.instanceOf(Function).isRequired,
+  onSubmit: PropTypes.instanceOf(Function).isRequired,
+  submitting: PropTypes.bool.isRequired,
+  value: PropTypes.string.isRequired,
+}
+
+const FeedBack = ({ feedback, user }) => {
+  const [comments, setComments] = useState(feedback)
+  const [submitting, setSubmitting] = useState(false)
+  const [value, setValue] = useState('')
+
+  const handleSubmit = () => {
+    if (!value) {
+      return
+    }
+
+    setSubmitting(true)
+
+    setTimeout(() => {
+      setComments([
+        ...comments,
+        {
+          author: user.login,
+          avatar: user.avatar_url,
+          content: <p>{value}</p>,
+          datetime: moment().fromNow(),
+        },
+      ])
+      setSubmitting(false)
+      setValue('')
+    }, 1000)
+  }
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  return (
+    <>
+      {comments.length > 0 && <CommentList comments={comments} />}
+      <Comment
+        avatar={<Avatar src={user.avatar_url} alt={user.login} />}
+        content={
+          <Editor
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            submitting={submitting}
+            value={value}
+          />
+        }
+      />
+    </>
+  )
+}
+
+FeedBack.propTypes = {
+  feedback: PropTypes.instanceOf(Array),
+  user: PropTypes.instanceOf(Object).isRequired,
+}
+
+FeedBack.defaultProps = {
+  feedback: [],
+}
+
+const mapStateToProps = (state) => ({
+  user: state.auth.user,
+})
+
+export default connect(mapStateToProps)(FeedBack)
